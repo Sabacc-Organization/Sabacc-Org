56804e51b01203f4850dc244b20030bb2cd01856
Heinoushare <samuel.sudbury@gmail.com>
2023-08-09 22:44:09 -0400

    removing useless files

--- static/index.js
+++
@@ -1,7 +0,0 @@
-// let gamesData = $('#games-data').data();
-// console.log(gamesData);
-// console.log(gamesData["games"]);
-
-let usersData = $('#users-data').data();
-console.log(usersData);
-console.log(usersData["usernames"][1]);

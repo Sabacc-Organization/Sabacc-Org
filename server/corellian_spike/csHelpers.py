78446786acfac1fe97bc3153e7793603b7f083fe
Ethan222 <ethannworks@gmail.com>
2024-04-14 00:06:41 -0400

    created separate directories for traditional and corellian spike

--- server/corellian_spike/csHelpers.py
+++ server/corellian_spike/csHelpers.py
@@ -1,6 +1,11 @@
 # corellian spike helper functions
 
 import random
+import sys
+import os
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
 from dataHelpers import *
 
 class Card:
--- server/traditional/alderaanHelpers.py
+++ server/traditional/alderaanHelpers.py
@@ -1,3 +1,8 @@
+import sys
+import os
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
 from helpers import *
 
 # Calculate the hand values in a list of hands

358db2716bcd6e0a26e1dee18ec083a47dd31c0b
Ethan222 <ethannworks@gmail.com>
2024-04-14 15:42:19 -0400

    made some more helper functions

--- server/corellian_spike/csHelpers.py
+++ server/corellian_spike/csHelpers.py
@@ -14,54 +14,122 @@ class Card:
         self.suit = suit
     def __str__(self):
         return f'{'+' if self.value > 0 else ''}{self.value} {self.suit}'
+    
+    @staticmethod
+    def listOfCardsToStr(cards):
+        ret = '['
+        for card in cards:
+            ret += str(card) + ', '
+        return str(ret).strip(', ') + ']'
 
 class Deck:
     def __init__(self):
-        self.constructDeck()
-    
-    def constructDeck(self):
         self.cards = []
         for suit in ['circle','square','triangle']:
             for val in range(1, 11):
                 self.cards.extend([Card(val, suit), Card(-val, suit)])
         sylop = Card(0, 'sylop')
         self.cards.extend([sylop, sylop])
+        self.shuffle()
     
     def __str__(self):
-        ret = []
-        for card in self.cards:
-            ret.append(str(card))
-        return str(ret)
+        return Card.listOfCardsToStr(self.cards)
 
-    def draw(self):
-        return self.cards.pop(random.randint(0, len(self.cards) - 1))
+    def shuffle(self):
+        for i in range(len(self.cards)):
+            switchIndex = random.randint(0, len(self.cards) - 1)
+            temp = self.cards[switchIndex]
+            self.cards[switchIndex] = self.cards[i]
+            self.cards[i] = temp
 
-# deal hands to each player
-def dealHands(deck, playerCount):
-    # Draw 2 cards for every hand
-    hands = []
-    for i in range(playerCount):
-        hands.append([deck.draw(), deck.draw()])
-    return hands
+    def draw(self, numCards=1):
+        if numCards == 1:
+            return self.cards.pop()
+        else:
+            drawnCards = self.cards[-numCards:]
+            del self.cards[-numCards:]
+            return drawnCards
 
-def newGame(players):
-    hPot = 0
-    sPot = 0
-    credits = ''
-    for i in range(len(players)):
-        credits += '985,'
-        hPot += 5
-        sPot += 10
-    playersStr = listToStr(players)
-    bets = (len(players) - 1) * ','
-    deck = Deck()
-    hands = dealHands(deck, len(players))
-    return {
-        'hPot': 0,
-        'sPot': 0,
-        'credits': credits,
-        'players': playersStr,
-        'bets': bets,
-        'deck': deck,
-        'hands': hands
-    }
+class Player:
+    def __init__(self, id):
+        self.id = id
+        self.hand = []
+        self.credits = 990
+        self.bet = 0
+    
+    def drawFromDeck(self, deck):
+        self.hand.append(deck.draw())
+    
+    def drawFromDiscard(self, discardPile):
+        self.hand.append(discardPile.pop()) # since new cards are added to the end, the last card is the top one
+    
+    def buyFromDeck(self, deck):
+        self.drawFromDeck(deck)
+        self.credits -= 5
+
+    def buyFromDiscard(self, discardPile):
+        self.drawFromDiscard(discardPile)
+        self.credits -= 10
+
+    def tradeWithDeck(self, deck, tradeCardIndex):
+        self.discard(tradeCardIndex)
+        self.drawFromDeck(deck)
+
+    def tradeWithDiscard(self, discardPile, tradeCardIndex):
+        self.hand.append(discardPile.pop())
+        self.discard(tradeCardIndex, discardPile)
+
+    def discard(self, discardCardIndex, discardPile):
+        discardPile.append(self.hand.pop(discardCardIndex))
+    
+    def getIndexOfCard(self, targetCard):
+        for i in range(len(self.hand)):
+            if self.hand[i] == targetCard:
+                return i
+        return -1
+        
+
+class CorellianSpikeGame:
+    def __init__(self, playerIds):
+        self.players = []
+        for id in playerIds:
+            self.players.append(Player(id))
+        self.newGame()
+        
+    def newGame(self):
+        # clear players' hands
+        for player in self.players:
+            player.hand = []
+        
+        # create deck, discard pile, and pots
+        self.deck = Deck()
+        self.discardPile = [self.deck.draw()]
+        self.handPot = 5 * len(self.players)
+        self.sabaccPot = 5 * len(self.players)
+
+        # deal 2 cards to each player
+        for i in range(2):
+            for player in self.players:
+                player.hand.append(self.deck.draw())
+        
+        # create discard pile with a card from the deck
+        self.discardPile = [self.deck.draw()]
+
+        self.round = 1 # round number
+
+        # designate the 1st player as the 1st dealer
+        self.dealerId = self.players[0].id
+    
+    def shift(self):
+        for player in self.players:
+            # put player's cards on bottom of deck
+            self.deck.cards = player.hand + self.deck.cards
+            # deal player as many cards as they had before
+            player.hand = self.deck.draw(len(player.hand))
+    
+    def __str__(self):
+        ret = f'\ndeck ({len(self.deck.cards)}): {self.deck}\n'
+        ret += f'discard pile ({len(self.discardPile)}): {Card.listOfCardsToStr(self.discardPile)}\n'
+        for player in self.players:
+            ret += f'player {player.id} hand: {Card.listOfCardsToStr(player.hand)}\n'
+        return ret

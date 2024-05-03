73bcd0d41228923d6e3fc6c0d25c2c20b6a89074
Heinoushare <samuel.sudbury@gmail.com>
2024-05-03 14:50:16 -0400

    Adding how to play, discord, and home page updates

--- client/.svelte-kit/generated/client/nodes/6.js
+++ client/.svelte-kit/generated/client/nodes/6.js
@@ -1,1 +1,1 @@
-export { default as component } from "../../../../src/routes/logout/+page.svelte";
+export { default as component } from "../../../../src/routes/login/+page.svelte";
--- client/.svelte-kit/generated/client/nodes/8.js
+++ client/.svelte-kit/generated/client/nodes/8.js
@@ -1,1 +1,1 @@
-export { default as component } from "../../../../src/routes/settings/+page.svelte";
+export { default as component } from "../../../../src/routes/register/+page.svelte";

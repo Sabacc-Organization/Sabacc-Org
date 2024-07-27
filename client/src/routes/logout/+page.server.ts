1a4ecd8d2de7a8753925f9f700dc10f1f56c6e82
Heinoushare <76819177+Heinoushare@users.noreply.github.com>
2024-07-26 23:30:25 -0400

    Adding correct options for cookie deletions

--- client/src/routes/logout/+page.server.ts
+++ client/src/routes/logout/+page.server.ts
@@ -3,9 +3,29 @@
 import { redirect } from "@sveltejs/kit";
 
 export async function load({ cookies }) {
-    cookies.delete('username', { path: '/' });
-    cookies.delete('password', { path: '/' });
-    cookies.delete('dark', { path: '/' });
-    cookies.delete('theme', { path: '/' });
-    cookies.delete('cardDesign', { path: '/' });
+    cookies.delete('username', {
+        path: '/',
+        httpOnly: false,
+        secure: false,
+    });
+    cookies.delete('password', {
+        path: '/',
+        httpOnly: false,
+        secure: false,
+    });
+    cookies.delete('dark', {
+        path: '/',
+        httpOnly: false,
+        secure: false,
+    });
+    cookies.delete('theme', {
+        path: '/',
+        httpOnly: false,
+        secure: false,
+    });
+    cookies.delete('cardDesign', {
+        path: '/',
+        httpOnly: false,
+        secure: false,
+    });
 }

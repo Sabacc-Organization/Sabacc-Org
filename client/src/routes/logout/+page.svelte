4e87649dfef720e12e8716c2f856876db2045150
Heinoushare <76819177+Heinoushare@users.noreply.github.com>
2024-07-26 23:27:28 -0400

    Using onMount to avoid window errors

--- client/src/routes/logout/+page.svelte
+++ client/src/routes/logout/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
     import { customRedirect } from '$lib';
+    import { onMount } from 'svelte';
     const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL;
 
-    customRedirect(FRONTEND_URL);
+    onMount( async () => {
+        customRedirect(FRONTEND_URL);
+    });
 
 </script>
